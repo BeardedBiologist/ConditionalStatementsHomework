9c1a2733e2df3934cf77b0496732425ad68ee77a
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-02-26 22:29:11 +0100

    Inital Commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿/* BREIF:
+ * 
+ * Create a Console Application that asks the user for their name. 
+ * Welcome me (Josh) as professor or anyone else as student. 
+ * Make sure that "JOSH" also gets called professor.
+ * 
+ * 
+ */
