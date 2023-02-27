ea148d51bcd4c73f721269707cf4565cfd361499
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-02-27 11:18:34 +0100

    finished application

--- Program.cs
+++ Program.cs
@@ -6,3 +6,21 @@
  * 
  * 
  */
+
+Console.WriteLine("Welcome to the Conditional Statement Homework App");
+Console.WriteLine();
+Console.WriteLine();
+
+Console.Write("What is your FIRST NAME ?: ");
+string? firstName = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine();
+
+if (firstName.ToLower() == "josh")
+{
+    Console.WriteLine("Welcome Professor!");
+}
+else
+{
+    Console.WriteLine("Welcome Student!");
+}
